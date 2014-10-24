0582abb6b3da859abab8ad59a26b97687575633d
fort <fort@mail.com>
2014-10-23 22:01:55 -0700

    jpa hibernate postgres working

--- src/main/java/net/foodmanager/Main.java
+++ src/main/java/net/foodmanager/Main.java
@@ -1,12 +1,25 @@
 package net.foodmanager;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  * @author fort
  */
 public class Main {
 
+    private static final String PERSISTENCE_UNIT = "foodb";
+
     public static void main(String[] args) {
         System.out.println("FoodManager started");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+        EntityManager em = emf.createEntityManager();
+
+        System.out.println("em: " + em);
+
+        System.exit(0);
     }
 
 }
