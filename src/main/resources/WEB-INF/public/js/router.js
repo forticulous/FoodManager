c0b41e01edcba344811beb96c3db6cf33cce1872
fort <fort@mail.com>
2014-11-04 19:27:58 -0800

    Basic template for food days and individual food day.  Trying to ignore js libs

--- src/main/resources/WEB-INF/public/js/router.js
+++ src/main/resources/WEB-INF/public/js/router.js
@@ -1,9 +1,26 @@
 FoodManager.Router.map(function() {
-    this.resource('foodDays', { path: '/' });
+    this.resource('foodDays', { path: '/days' });
+    this.resource('foodDay', { path: '/days/:localDate' });
 });
+
+FoodManager.IndexRoute = Ember.Route.extend({
+    redirect: function() {
+        this.transitionTo('foodDays');
+    }
+})
 
 FoodManager.FoodDaysRoute = Ember.Route.extend({
     model: function() {
         return this.store.find('foodDay');
+    }
+});
+
+FoodManager.FoodDayRoute = Ember.Route.extend({
+    model: function(params) {
+        return {
+            id: 1,
+            localDate: params.localDate,
+            calories: 2000
+        };
     }
 });
