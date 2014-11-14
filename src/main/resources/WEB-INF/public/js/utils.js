30c1120da9a0432fe420e1027c7a0fff975a814d
fort <fort@mail.com>
2014-11-13 17:10:09 -0800

    Trying to precompile handlebars templates

--- src/main/resources/WEB-INF/public/js/utils.js
+++ src/main/resources/WEB-INF/public/js/utils.js
@@ -13,9 +13,42 @@ FoodManager.Utils = Ember.Object.extend({
     },
     isUnset: function(obj) {
         return obj === undefined || obj.length === 0;
+    },
+    compileTemplate: function(path, name) {
+        Ember.$.ajax(path, {
+            async: false,
+            success: function(templateText) {
+                var template = Ember.Handlebars.compile(templateText);
+                Ember.TEMPLATES[name] = template;
+            }
+        });
+    },
+    compileManyTemplates: function(paths, names) {
+        for (var i = 0; i < paths.length; i++) {
+            this.compileTemplate(paths[i], names[i]);
+        }
     }
 });
 
 FoodManager.reopen({
     utils: FoodManager.Utils.create()
 });
+
+// Load templates
+(function() {
+    var paths = [
+        'templates/foodDayIndex.handlebars',
+        'templates/foodDayItemsIndex.handlebars',
+        'templates/foodDayItemsNew.handlebars',
+        'templates/foodDaysIndex.handlebars',
+        'templates/foodDaysNew.handlebars'
+    ];
+    var names = [
+        'foodDay/index',
+        'foodDayItems/index',
+        'foodDayItems/new',
+        'foodDays/index',
+        'foodDays/new'
+    ];
+    FoodManager.utils.compileManyTemplates(paths, names);
+})();
