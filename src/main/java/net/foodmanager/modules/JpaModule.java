882f9c868b12aca763739cf5f762270a505ce6b2
fort <fort@mail.com>
2014-11-19 22:44:48 -0800

    Should allow for local and hosted db connections

--- src/main/java/net/foodmanager/modules/JpaModule.java
+++ src/main/java/net/foodmanager/modules/JpaModule.java
@@ -5,6 +5,9 @@ import net.foodmanager.util.JpaUtil;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 /**
  * @author fort
@@ -20,8 +23,31 @@ public class JpaModule extends AbstractModule {
     }
 
     private EntityManagerFactory provideEntityManagerFactory() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+        String databaseUrl = System.getenv("DATABASE_URL");
+        StringTokenizer st = new StringTokenizer(databaseUrl, ":@/");
+        boolean local = "jdbc".equals(st.nextToken());
+        Map<String, String> properties = local ? localProps(databaseUrl) : hostedProps(databaseUrl, st);
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT, properties);
         return emf;
+    }
+
+    private Map<String, String> hostedProps(String databaseUrl, StringTokenizer st) {
+        String userName = st.nextToken();
+        String password = st.nextToken();
+
+        Map<String, String> properties = new HashMap<>();
+        properties.put("javax.persistence.jdbc.url", databaseUrl);
+        properties.put("javax.persistence.jdbc.user", userName);
+        properties.put("javax.persistence.jdbc.password", password);
+        return properties;
+    }
+
+    private Map<String, String> localProps(String databaseUrl) {
+        Map<String, String> properties = new HashMap<>();
+        properties.put("javax.persistence.jdbc.url", databaseUrl);
+        properties.put("javax.persistence.jdbc.user", "postgres");
+        return properties;
     }
 
 }
