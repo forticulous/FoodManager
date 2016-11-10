e093a3c75ec47f3b410ee2d57fe31d3e65a474a6
fort <fort@tmail.com>
2016-11-10 14:19:01 -0800

    Add foreign key constraint to database definition

--- database_structure.sql
+++ database_structure.sql
@@ -9,6 +9,6 @@ CREATE TABLE food_day_item (
   food_day_item_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   food_desc TEXT NOT NULL,
   meal TEXT NOT NULL,
-  food_day_id UUID NOT NULL,
+  food_day_id UUID REFERENCES food_day(food_day_id) NOT NULL,
   calories INTEGER NOT NULL
 );
