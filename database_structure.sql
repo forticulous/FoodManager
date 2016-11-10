aa682df8ec6b1718d57b5ee1701abdd9f424f9e6
fort <fort@tmail.com>
2016-11-10 14:13:00 -0800

    Add sql file to recreate database structure, modify gradle to pass along system properties, simplify database connection code

---
+++ database_structure.sql
@@ -0,0 +1,14 @@
+CREATE EXTENSION pgcrypto;
+
+CREATE TABLE food_day (
+  food_day_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  local_date TEXT NOT NULL
+);
+
+CREATE TABLE food_day_item (
+  food_day_item_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  food_desc TEXT NOT NULL,
+  meal TEXT NOT NULL,
+  food_day_id UUID NOT NULL,
+  calories INTEGER NOT NULL
+);
--- src/main/java/net/foodmanager/modules/JpaModule.java
+++ src/main/java/net/foodmanager/modules/JpaModule.java
@@ -7,7 +7,6 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.StringTokenizer;
 
 /**
  * @author fort
@@ -23,38 +22,21 @@ public class JpaModule extends AbstractModule {
     }
 
     private EntityManagerFactory provideEntityManagerFactory() {
-        String databaseUrl = System.getenv("DATABASE_URL");
-        StringTokenizer st = new StringTokenizer(databaseUrl, ":@/");
-        boolean local = Boolean.TRUE.toString().equals(System.getProperty("localDev", Boolean.FALSE.toString()));
-        Map<String, String> properties = local ? localProps(st) : hostedProps(st);
+        String connectionString = System.getProperty("connectionString");
+        String[] st = connectionString.split("[:@/]");
+        Map<String, String> properties = databaseProps(st);
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT, properties);
         return emf;
     }
 
-    private Map<String, String> hostedProps(StringTokenizer st) {
-        st.nextToken(); // provider
-        String userName = st.nextToken();
-        String password = st.nextToken();
-        String host = st.nextToken();
-        String port = st.nextToken();
-        String databaseName = st.nextToken();
+    private Map<String, String> databaseProps(String[] st) {
+        String userName = st[0];
+        String password = st[1];
+        String host = st[2];
+        String port = st[3];
+        String databaseName = st[4];
         String jdbcUrl = String.format("jdbc:postgresql://%s:%s/%s", host, port, databaseName);
-
-        Map<String, String> properties = new HashMap<>();
-        properties.put("javax.persistence.jdbc.url", jdbcUrl);
-        properties.put("javax.persistence.jdbc.user", userName);
-        properties.put("javax.persistence.jdbc.password", password);
-        return properties;
-    }
-
-    private Map<String, String> localProps(StringTokenizer st) {
-        st.nextToken(); // provider
-        String userName = st.nextToken();
-        String password = st.nextToken();
-        String host = st.nextToken();
-        String databaseName = st.nextToken();
-        String jdbcUrl = String.format("jdbc:postgresql://%s/%s", host, databaseName);
 
         Map<String, String> properties = new HashMap<>();
         properties.put("javax.persistence.jdbc.url", jdbcUrl);
